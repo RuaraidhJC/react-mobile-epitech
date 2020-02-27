5d3f5b1ca6198c4ce56642ef11fc89f55ee1770a
Patrick BUI <patrick.bui@epitech.eu>
2020-02-27 18:17:28 +0100

    add friend cards
    Signed-off-by: Patrick BUI <patrick.bui@epitech.eu>

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -2,17 +2,56 @@ import React from 'react'
 import { View,
     Text,
     StyleSheet,
-    Button
+    Button,
+    ScrollView,
+    Dimensions,
+    Platform,
+    TouchableOpacity
 } from 'react-native'
-//import { connect } from 'react-redux'
-//import Firebase from '../config/Firebase'
-import {LinearGradient} from 'expo';
-import {primaryGradientArray} from "../utils/Colors";
-//import SubTitle from "../app/components/SubTitle";
 
+// Galio components
+import {
+    Card, Block, NavBar, Icon
+} from 'galio-framework';
+import theme from '../utils/theme';
 
 import Storage from '../utils/Storage';
 import Network from '../utils/Network';
+
+
+const cards = [
+    {
+        id: 1,
+        avatar: 'http://i.pravatar.cc/100',
+        title: 'Christopher Moon',
+        caption: '138 minutes ago',
+        location: 'Los Angeles, CA',
+    },
+    {
+        id: 2,
+        avatar: 'http://i.pravatar.cc/100',
+        title: 'Christopher Moon',
+        caption: '138 minutes ago',
+        location: 'Los Angeles, CA',
+    },
+    {
+        id: 3,
+        avatar: 'http://i.pravatar.cc/100',
+        title: 'Christopher Moon',
+        caption: '138 minutes ago',
+        location: 'Los Angeles, CA',
+        padded: true,
+    },
+    {
+        id: 4,
+        avatar: 'http://i.pravatar.cc/100',
+        title: 'Christopher Moon',
+        caption: '138 minutes ago',
+        location: 'Los Angeles, CA',
+        padded: true,
+    },
+];
+
 
 export default class Profile extends React.Component {
     state = {
@@ -61,16 +100,43 @@ export default class Profile extends React.Component {
 
         return (
             <View style={styles.container}>
+                <Block style={styles.container}>
                 <Text style={styles.title}> Profile </Text>
                 <Text style={styles.big}> {email} </Text>
                 <Text>{'\n'}</Text>
                 <Button title='Logout' onPress={this.handleSignout} />
                 <Text>{'\n'}</Text>
                 <Button title='Map' onPress={this.handleMaps} />
+                </Block>
+
+                <ScrollView contentContainerStyle={styles.cards}>
+                    <Block flex space="between">
+                        {cards && cards.map((card, id) => (
+                            <Card
+                                key={`card-${card.image}`}
+                                flex
+                                borderless
+                                shadowColor={theme.COLORS.BLACK}
+                                titleColor={card.full ? theme.COLORS.WHITE : null}
+                                style={styles.card}
+                                title={card.title}
+                                caption={card.caption}
+                                location={card.location}
+                                avatar={`${card.avatar}?${id}`}
+                                footerStyle={card.full ? styles.full : null}
+                            >
+                                {card.full ? <LinearGradient colors={['transparent', 'rgba(0,0,0, 0.8)']} style={styles.gradient} /> : null}
+                            </Card>
+                        ))}
+                    </Block>
+                </ScrollView>
             </View>
+
         )
     }
 }
+
+const { width } = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
     container: {
@@ -86,6 +152,41 @@ const styles = StyleSheet.create({
     big: {
         fontSize: 20,
         color: '#000'
-    }
+    },
+    cards: {
+        width,
+        backgroundColor: theme.COLORS.WHITE,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    card: {
+        backgroundColor: theme.COLORS.WHITE,
+        width: width - theme.SIZES.BASE * 2,
+        marginVertical: theme.SIZES.BASE * 0.875,
+        elevation: theme.SIZES.BASE / 2,
+    },
+    full: {
+        position: 'absolute',
+        bottom: 0,
+        right: 0,
+        left: 0,
+    },
+    noRadius: {
+        borderBottomLeftRadius: 0,
+        borderBottomRightRadius: 0,
+    },
+    rounded: {
+        borderRadius: theme.SIZES.BASE * 0.1875,
+    },
+    gradient: {
+        bottom: 0,
+        left: 0,
+        right: 0,
+        height: 90,
+        position: 'absolute',
+        overflow: 'hidden',
+        borderBottomRightRadius: theme.SIZES.BASE * 0.5,
+        borderBottomLeftRadius: theme.SIZES.BASE * 0.5,
+    },
 })
 
