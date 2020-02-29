3255b4648afe00688cca28a68b94f2f170a9a6f2
Patrick BUI <patrick.bui@epitech.eu>
2020-03-01 00:32:13 +0100

    added friends request and list
    Signed-off-by: Patrick BUI <patrick.bui@epitech.eu>

--- screens/Profile.jsx
+++ screens/Profile.jsx
@@ -2,22 +2,23 @@ import React from 'react'
 import { View,
     Text,
     StyleSheet,
-    Button,
     ScrollView,
     Dimensions,
     Platform,
     TouchableOpacity
 } from 'react-native'
 
+import { LinearGradient } from 'expo-linear-gradient';
+
 // Galio components
 import {
-    Card, Block, NavBar, Icon
+    Card, Block, NavBar, Icon, Button
 } from 'galio-framework';
 import theme from '../utils/theme';
 
 import Storage from '../utils/Storage';
 import Network from '../utils/Network';
-
+import GestureRecognizer from "react-native-swipe-gestures";
 
 const cards = [
     {
@@ -26,6 +27,7 @@ const cards = [
         title: 'Christopher Moon',
         caption: '138 minutes ago',
         location: 'Los Angeles, CA',
+        friends: true,
     },
     {
         id: 2,
@@ -33,6 +35,8 @@ const cards = [
         title: 'Christopher Moon',
         caption: '138 minutes ago',
         location: 'Los Angeles, CA',
+        friends: true,
+
     },
     {
         id: 3,
@@ -40,6 +44,7 @@ const cards = [
         title: 'Christopher Moon',
         caption: '138 minutes ago',
         location: 'Los Angeles, CA',
+        friends: true,
         padded: true,
     },
     {
@@ -48,7 +53,28 @@ const cards = [
         title: 'Christopher Moon',
         caption: '138 minutes ago',
         location: 'Los Angeles, CA',
+        friends: false,
         padded: true,
+    },
+];
+
+const cardsreq = [
+    {
+        id: 1,
+        avatar: 'http://i.pravatar.cc/100',
+        title: 'Monique Moon',
+        caption: '138 minutes ago',
+        location: 'Los Angeles, CA',
+        friends: false,
+    },
+    {
+        id: 2,
+        avatar: 'http://i.pravatar.cc/100',
+        title: 'Jacqueline Moon',
+        caption: '138 minutes ago',
+        location: 'Los Angeles, CA',
+        friends: false,
+
     },
 ];
 
@@ -75,18 +101,24 @@ export default class Profile extends React.Component {
 
     getUserData = async () => {
         this.setState({email: await Storage.getEmail()});
-    }
+    };
 
     handleMaps = () => {
         this.props.navigation.navigate('homeScreen')
-    }
+    };
 
     handleSignout = async () => {
         Storage.logout();
         this.props.navigation.navigate('loginScreen')
-    }
+    };
 
+    addFriend = (id) => {
+        console.log("adding friend id: " + id)
+    };
 
+    refuseFriend = (id) => {
+        console.log("refusing friend id: " + id)
+    };
 
 
     render() {
@@ -101,16 +133,48 @@ export default class Profile extends React.Component {
         return (
             <View style={styles.container}>
                 <Block style={styles.container}>
-                <Text style={styles.title}> Profile </Text>
-                <Text style={styles.big}> {email} </Text>
-                <Text>{'\n'}</Text>
-                <Button title='Logout' onPress={this.handleSignout} />
-                <Text>{'\n'}</Text>
-                <Button title='Map' onPress={this.handleMaps} />
-                </Block>
+                    <View style={{flexDirection: "row"}}>
+
+                    <Button
+                        style={[styles.buttonl]}
+                        uppercase
+                        shadowless
+                        size="small"
+                        color="rgb(206, 102, 89)"
+                        onPress={this.handleSignout}
+                    >
+                        Logout
+                    </Button>
+
+                        <Text style={styles.title}> Profile </Text>
+
+                    <Button
+                        style={[styles.buttonr]}
+                        uppercase
+                        shadowless
+                        size="small"
+                        color="rgb(0, 177, 238)"
+                        onPress={this.handleMaps}
+                    >
+                        Map
+                    </Button>
+                    </View>
 
                 <ScrollView contentContainerStyle={styles.cards}>
+
                     <Block flex space="between">
+                        <Card
+                            flex
+                            borderless
+                            shadowColor={theme.COLORS.BLACK}
+                            style={styles.card}
+                            title="Christopher Moon"
+                            caption="test@test.com"
+                            avatar="http://i.pravatar.cc/100?id=pineaple"
+                        />
+
+                        <Text style={styles.title}> Friends </Text>
+
                         {cards && cards.map((card, id) => (
                             <Card
                                 key={`card-${card.image}`}
@@ -128,8 +192,47 @@ export default class Profile extends React.Component {
                                 {card.full ? <LinearGradient colors={['transparent', 'rgba(0,0,0, 0.8)']} style={styles.gradient} /> : null}
                             </Card>
                         ))}
+
+                        {cardsreq && cardsreq.map((card, id) => (
+                            <Card
+                                key={`card-${card.image}`}
+                                flex
+                                borderless
+                                shadowColor={theme.COLORS.GREY}
+                                titleColor={card.full ? theme.COLORS.WHITE : null}
+                                style={styles.card}
+                                title={card.title}
+                                avatar={`${card.avatar}?${id}`}
+                                footerStyle={card.full ? styles.full : null}
+                            >
+                                {card.full ? <LinearGradient colors={['transparent', 'rgba(0,0,0, 0.8)']} style={styles.gradient} /> : null}
+                                <View style={{flexDirection: "row"}}>
+                                    <Button
+                                        style={[styles.button]}
+                                        uppercase
+                                        shadowless
+                                        size="small"
+                                        color="rgb(176, 30, 11)"
+                                        onPress={() => this.refuseFriend(id)}
+                                    >
+                                        Refuse
+                                    </Button>
+                                    <Button
+                                        style={[styles.button]}
+                                        uppercase
+                                        shadowless
+                                        size="small"
+                                        color="rgb(29, 145, 31)"
+                                        onPress={() => this.addFriend(id)}
+                                    >
+                                        Accept
+                                    </Button>
+                                </View>
+                            </Card>
+                        ))}
                     </Block>
                 </ScrollView>
+                </Block>
             </View>
 
         )
@@ -140,18 +243,37 @@ const { width } = Dimensions.get('screen');
 
 const styles = StyleSheet.create({
     container: {
+        marginTop: 20,
         flex: 1,
         alignItems: 'center',
         justifyContent: 'center'
     },
     title: {
+        alignSelf: 'center',
         fontSize: 30,
         fontWeight: 'bold',
-        color: '#1e1e1e'
+        color: '#1e1e1e',
+        marginBottom: 20,
     },
     big: {
         fontSize: 20,
         color: '#000'
+    },
+    bigdown: {
+        fontSize: 25,
+        color: '#000',
+        marginTop: 20,
+    },
+    button: {
+        width: (width - theme.SIZES.BASE * 2) / 2,
+    },
+    buttonl: {
+        right: '60%',
+        width: 100,
+    },
+    buttonr: {
+        left: '60%',
+        width: 100,
     },
     cards: {
         width,
@@ -160,6 +282,18 @@ const styles = StyleSheet.create({
         justifyContent: 'flex-start',
     },
     card: {
+        backgroundColor: theme.COLORS.WHITE,
+        width: width - theme.SIZES.BASE * 2,
+        marginVertical: theme.SIZES.BASE * 0.875,
+        elevation: theme.SIZES.BASE / 2,
+    },
+    carding: {
+        width,
+        backgroundColor: theme.COLORS.WHITE,
+        alignItems: 'center',
+        justifyContent: 'flex-start',
+    },
+    cardsolo: {
         backgroundColor: theme.COLORS.WHITE,
         width: width - theme.SIZES.BASE * 2,
         marginVertical: theme.SIZES.BASE * 0.875,
